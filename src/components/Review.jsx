644cd5229cb0d5d115d3ed25b0ba56652fe434bf
Youngs-kim <doppio0518@gmail.com>
2022-08-04 14:46:17 -0700

    worked on review section with dropdown menu

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -8,8 +8,17 @@ const Review = () => {
     <div className="review-container">
       <form className="review">
         <input type="text" value={review} />
+        <label>Rating</label>
+        <select>
+          <option>5/5</option>
+          <option>4/5</option>
+          <option>3/5</option>
+          <option>2/5</option>
+          <option>1/5</option>
+        </select>
         <button>Add</button>
       </form>
+      <ul></ul>
     </div>
   );
 };
