7d9f37f9c7f3fc29a636d951b39af43bd71aee28
Youngs-kim <doppio0518@gmail.com>
2022-11-21 15:59:15 -0800

    modified all around design

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,7 +13,7 @@ const Menu = () => {
         <div>
           <h5 className="food-title">Bulgogi Rice</h5>
           <h6 className="price">$14.99</h6>
-          <h6>
+          <h6 className="menu-description">
             Thinly sliced beef marinated in a sauce that is a mix of soy sauce,
             sugar or honey, sesame oil, garlic, onion, and pureed Asian pear.
           </h6>
@@ -24,7 +24,7 @@ const Menu = () => {
         <div>
           <h5 className="food-title">Spicy Pork Rice</h5>
           <h6 className="price">$14.99</h6>
-          <h6>
+          <h6 className="menu-description">
             Spicy Pork- tender pieces of pork tenderloin are marinated and then
             coated in a sweet, spicy, and salty sauce. Serve over white rice for
             an unbelievably delicious dinner with plenty of heat.
@@ -36,7 +36,7 @@ const Menu = () => {
         <div>
           <h5 className="food-title">Bibimbap</h5>
           <h6 className="price">$13.99</h6>
-          <h6>
+          <h6 className="menu-description">
             Bibimbap (비빔밥), which literally means “mixed rice”, is one of
             Korea’s most famous dishes and it is made up of steamed rice topped
             with stir fried vegetables, meat and a fried (or raw) egg and
@@ -49,7 +49,7 @@ const Menu = () => {
         <div>
           <h5 className="food-title">Bulgogi Quesadilla</h5>
           <h6 className="price">$14.99</h6>
-          <h6>
+          <h6 className="menu-description">
             Korean fusion Monterey cheese Quesadilla with our home-made bulgogi.
             Enjoy with our Home-made Pico de gallo, salsa verde and seasame
             mayo.
@@ -61,7 +61,7 @@ const Menu = () => {
         <div>
           <h5 className="food-title">Korean Fried Chicken</h5>
           <h6 className="price">$17.99</h6>
-          <h6>
+          <h6 className="menu-description">
             8pcs of Korean Style Chicken Wings. Light and crispy with an added
             kick.
           </h6>
